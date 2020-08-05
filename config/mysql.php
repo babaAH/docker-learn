79ac9ef75ec0c83508515cd2c4d77b17941c4c2a
babaAH <shama_chich@icloud.com>
2020-08-05 19:19:42 +0400

    nginx

---
+++ config/mysql.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => yii\db\Connection::class,
+    'dsn' => 'mysql:host=127.0.0.1;dbname=mysql_db',
+    'username' => 'root',
+    'password' => 'password',
+    'charset' => 'utf8',
+];
